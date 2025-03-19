1434d481a0f5bdd380c9fc647507e01e782573f1
Pablo Reimúndez <pablo.reimundezsanz@gmail.com>
2025-03-19 14:03:32 +0100

    Initial commit

---
+++ src/Departamento.java
@@ -0,0 +1,15 @@
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+public class Departamento {
+    private String nombre;
+    private int numEmpleados;
+    private Empleado[] empleados = new Empleado[2];
+    void anadir(Empleado v){
+        empleados[0]=v;
+    }
+}
---
+++ src/Empleado.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Empleado {
+    private String nombre;
+    private int edad;
+    private double sueldo;
+
+}
